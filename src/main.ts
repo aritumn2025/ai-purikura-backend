5597807c8b2e0ba1344be8b83338a92634b83886
yonep3904 <s59201@ga.ariake-nct.ac.jp>
2025-10-19 01:13:37 +0900

    swaggerの設定の追加

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Swagger設定
+  const config = new DocumentBuilder()
+    .setTitle('AI Purikura API')
+    .setDescription('AIプリクラシステムのバックエンドAPI仕様')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
+
   await app.listen(process.env.PORT ?? 4000);
 }
 bootstrap();
