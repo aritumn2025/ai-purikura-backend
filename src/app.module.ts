060011e15cbb3975c61cc43fda9d5c334a144355
yonep3904 <s59201@ga.ariake-nct.ac.jp>
2025-10-19 12:08:23 +0900

    add style module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { StyleModule } from './style/style.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, StyleModule],
   controllers: [AppController],
   providers: [AppService],
 })
